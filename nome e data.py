099a0b6b377cd915827ebd353ee9da822895fc34
naiararj <102701022+naiararj@users.noreply.github.com>
2022-04-22 16:13:55 -0300

    Add files via upload

---
+++ nome e data.py
@@ -0,0 +1,12 @@
+import datetime
+
+n = str(input('digite seu nome:')).strip()
+nome = n. split()
+print(n. upper())
+ano = eval(input('nasceu em que ano:'))
+mês= eval(input('nasceu em que mês:'))
+dia = eval(input('nasceu em que dia:'))
+date = ('ano = 1991, mês = 5, dia = 21')
+txt = (input('Ana nasceu no dia:'))
+print(txt . format(datetime.date(1991,5,21)))
+
